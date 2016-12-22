e433c6465ed1e3b24c07ae058999cf7705bc362d
Behardian <atlasblinked@gmail.com>
2016-12-22 15:47:53 +1300

    Add files via upload

---
+++ Controllers/Api/CustomersController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+
+        // Access Database
+        private ApplicationDbContext _context;
+
+        // Link Database to Controller
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/customers
+        public IHttpActionResult GetCustomers(string query = null)
+        {
+            var customersQuery = _context.Customers
+                .Include(c => c.MembershipType);
+
+            if (!string.IsNullOrWhiteSpace(query))
+                customersQuery = customersQuery.Where(c => c.Name.Contains(query));
+
+            var customerDtos = customersQuery
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
+
+            return Ok(customerDtos);
+        }
+
+        // GET /api/customers/1
+        public IHttpActionResult GetCustomer(int id)
+        {
+            // grab customer Id from database
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            // If customer is not found return standard Http response
+            if (customer == null)
+                return NotFound();
+
+            // If customer Id is found, return the value in the customer variable
+            return Ok(Mapper.Map<Customer, CustomerDto>(customer));
+        }
+
+        // POST /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
+        {
+            // Validate the object. If model state is not valid throw Http Bad Request Exception
+            if (!ModelState.IsValid)
+               return BadRequest();
+
+            // If state is valid then add customer to database
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto); // Pass CustomerDto data to variable then add to database
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            // Update Customer details with user-entered details
+            Mapper.Map(customerDto, customerInDb);
+            
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE /api/customers/id
+        [HttpDelete]
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                return NotFound();
+
+            // This object will be marked to be removed from memory
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/Api/NewRentalController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using System.Data.Entity;
+using Vidly.Dtos;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            // Check to see if the user has selected any movies
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No Movie Ids have been given");
+
+            // Obtain customer ID from the database
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+            
+            // Check to see if customer ID is valid
+            if (customer == null)
+                return BadRequest("Customer ID is not valid");
+
+            // Using the ID/s entered as parameters from newRental put all movies from the DB with matching ID's into a list
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            // Check to see if the Movies selected are valid
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more MovieIds are invalid");
+
+            foreach (var movie in movies)
+            {
+                // Check to see if the movies ordered are available
+                if(movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
+                movie.NumberAvailable--;
+
+                // Create new rental object for each movie
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                // Add new object to database
+                _context.Rentals.Add(rental);
+            }
+            // Save Changes
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
